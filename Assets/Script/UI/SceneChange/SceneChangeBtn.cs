043efe4637f5ccf913551d2335e720a9bdf10ccc
kimhakdo <plota0908@naver.com>
2025-02-26 10:48:50 +0900

    [Style] UI이미지 넣음

--- Assets/Script/UI/SceneChange/SceneChangeBtn.cs
+++ Assets/Script/UI/SceneChange/SceneChangeBtn.cs
@@ -5,11 +5,13 @@ using UnityEngine.SceneManagement; // 씬 매니저 사용
 
 public class SceneChangeBtn : MonoBehaviour
 {
+        // 게임씬 이동
     public void StartGameScene()
     {
         SceneManager.LoadScene("Test_DunGeon_Scene"); 
     }
 
+        // 메인씬 이동
     public void GoHomeScene()
     {
         SceneManager.LoadScene("Main_Scene");
