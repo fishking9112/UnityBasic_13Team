1ed91db36a7088103ae90b9f5623e2517e42d76a
fishking9112 <fishking9112@gmail.com>
2025-02-27 14:17:40 +0900

    [Fix] 코드 수정

--- Assets/Script/Controller/BaseController.cs
+++ Assets/Script/Controller/BaseController.cs
@@ -6,18 +6,11 @@ public class BaseController : MonoBehaviour
 {
     protected Rigidbody _rigidbody;
 
-    //[SerializeField] private SpriteRenderer CharacterRenderer;
-    //[SerializeField] private SkinnedMeshRenderer CharacterRenderer;
-    //[SerializeField] private Transform weaponPivot;
-
     protected Vector3 movementDirection = Vector3.zero;
     public Vector3 MovementDirection { get { return movementDirection; } }
 
     protected Vector3 lookDirection = Vector3.zero;
     public Vector3 LookDirection { get { return lookDirection; } }
-
-    private Vector3 knockback = Vector3.zero;
-    private float knockbackDuration = 0.0f;
 
     protected AnimationHandler animationHandler;
     protected StatHandler statHandler;
@@ -33,14 +26,7 @@ public class BaseController : MonoBehaviour
         animationHandler = GetComponent<AnimationHandler>();
         statHandler = GetComponent<StatHandler>();
 
-        //if (WeaponPrefab != null)
-        //{
-        //    weaponHandler = Instantiate(WeaponPrefab, weaponPivot);
-        //}
-        //else
-        //{
         weaponHandler = GetComponentInChildren<WeaponHandler>();
-        //}
     }
 
     protected virtual void Start()
@@ -58,10 +44,6 @@ public class BaseController : MonoBehaviour
     {
         MoveMent(MovementDirection);
 
-        if (knockbackDuration > 0.0f)
-        {
-            knockbackDuration -= Time.fixedDeltaTime;
-        }
     }
     protected virtual void HandleAction()
     {
@@ -86,13 +68,6 @@ public class BaseController : MonoBehaviour
 
         _rigidbody.rotation= Quaternion.Euler(0f, rotZ,0f );
     }
-
-    public void Applyknockback(Transform other, float power, float duration)
-    {
-        knockbackDuration = duration;
-        knockback = -(other.position - transform.position).normalized * power;
-    }
-
 
 
     protected virtual void Attack()
